71d3a34e0959e34f800398a7902add42dcfbcac8
root <root@csz25042.canlab.ibm.com>
2017-02-08 20:53:49 +0000

    fix dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,8 @@ FROM s390x/ubuntu:16.04
 
 COPY *.sh *.bin *.patch /RthDB/
 
+EXPOSE 29015 28015 8080
+
 RUN cd /RthDB && ./RethinkDB_Buiding.sh
+
+#CMD ["rethinkdb","--bind","all"]
--- install_node.sh
+++ install_node.sh
@@ -1,5 +1,3 @@
-chmod +x ibm-1.2.0.17-node-v0.12.18-linux-s390x.bin
 printf '5\n\n1\n\n\n\n\n\n' | ./ibm-1.2.0.17-node-v0.12.18-linux-s390x.bin
-export PATH=$PATH:/root/ibm/node/bin
 npm install -g coffee-script@1.12.3
 npm install -g browserify@12.0.1
